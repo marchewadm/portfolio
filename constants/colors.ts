8512e56cc862e3b0c356050c60134f1b1f87c663
marchewadm <merchwa.dawid@gmail.com>
2024-12-05 23:53:34 +0100

    feat: add color constants

---
+++ constants/colors.ts
@@ -0,0 +1,13 @@
+export const COLORS = {
+  "primary": "#1A1A1A",
+  "foreground": "#333333",
+  "foreground-light": "#666666",
+
+  "border-light": "#CCCCCC",
+  "border-input": "#E6E6E6",
+
+  "surface": "#FFFFFF",
+  "surface-footer": "#FBFBFB",
+
+  "text-on-primary": "#FCFCFC",
+} as const;
