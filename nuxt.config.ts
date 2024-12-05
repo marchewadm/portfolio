6ee40b135cb62e9cc2b445b8a9ebbf57279bdf82
marchewadm <merchwa.dawid@gmail.com>
2024-12-06 00:54:51 +0100

    feat: integrate Tailwind CSS and custom font configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { COLORS } from "./constants/colors";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
@@ -9,4 +10,27 @@ export default defineNuxtConfig({
     "@nuxt/icon",
     "@nuxt/image",
   ],
+  fonts: {
+    defaults: {
+      weights: [100, 300, 400, 500],
+      styles: ["normal"],
+    },
+    families: [{ name: "Inter", provider: "google" }],
+  },
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          colors: COLORS,
+          boxShadow: {
+            xs: "0px -3px 10px 0px rgba(102, 102, 102, 25%)",
+          },
+        },
+        fontFamily: {
+          custom: ["Inter", "sans-serif"],
+        },
+      },
+    },
+  },
+  css: ["@/assets/css/index.css"],
 });
