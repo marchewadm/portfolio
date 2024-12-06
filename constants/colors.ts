d297504d8bd9d84be0b510d6fdaa7fdcfb877499
marchewadm <merchwa.dawid@gmail.com>
2024-12-06 23:30:17 +0100

    feat: new color constant & improve naming, include comments for clarity

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,13 +1,18 @@
 export const COLORS = {
+  // Used for primary buttons, links, text, etc.
   "primary": "#1A1A1A",
   "foreground": "#333333",
-  "foreground-light": "#666666",
+  "foreground-lighter": "#666666",
 
-  "border-light": "#CCCCCC",
-  "border-input": "#E6E6E6",
+  // Used for borders, lines, dividers, etc.
+  "decorative": "#CCCCCC",
+  "decorative-lighter": "#E6E6E6",
+  "decorative-lightest": "#f0f0f0",
 
+  // Used for background of sections, cards, etc.
   "surface": "#FFFFFF",
-  "surface-footer": "#FBFBFB",
+  "surface-darker": "#FBFBFB",
 
-  "text-on-primary": "#FCFCFC",
+  // Used for text on primary background
+  "on-primary": "#FCFCFC",
 } as const;
