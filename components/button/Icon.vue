bf5ac1bd89da0c39211c66f13ca7742db485d604
marchewadm <merchwa.dawid@gmail.com>
2024-12-06 12:36:14 +0100

    feat: add `ButtonIcon` & `IconBase` components, constants, and types
    - Added `ButtonIcon` component to display a button with an icon, accepting an `IconName` prop.
    - Added `IconBase` component to render icons dynamically using the `ICON_PROVIDER` constant.
    - Introduced `ICON_PROVIDER` constant in `constants/icons.ts` for centralized icon provider configuration.
    - Added `IconName` type in `types/icon.ts` for stricter type checking of icon names.

---
+++ components/button/Icon.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { IconName } from "~/types/icon";
+
+defineProps<IconName>();
+</script>
+
+<template>
+  <button class="flex items-center justify-center">
+    <IconBase :name="name" />
+  </button>
+</template>
---
+++ components/icon/Base.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { IconName } from "~/types/icon";
+
+const props = defineProps<IconName>();
+
+const getIconName = computed(() => {
+  return `${ICON_PROVIDER}:${props.name}`;
+});
+</script>
+
+<template>
+  <Icon
+    :name="getIconName"
+    class="h-6 w-6"
+  />
+</template>
