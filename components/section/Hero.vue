aad8c77ac04a9a0f1ffdaa4d692822b9aad3f262
marchewadm <merchwa.dawid@gmail.com>
2024-12-06 23:47:56 +0100

    feat: add `SectionHero` component with introductory content

---
+++ components/section/Hero.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="relative flex h-[350px] items-center overflow-hidden">
+    <div class="z-10 px-4">
+      <h1 class="mb-1 text-2xl font-medium">
+        Dawid Merchwa
+      </h1>
+      <h2 class="mb-2 font-medium">
+        A Computer Science student
+      </h2>
+      <p class="mb-4 text-sm font-light">
+        Passionate developer, open source contributor, Linux enthusiast, and a lifelong learner exploring new technologies.
+      </p>
+      <ButtonVariant
+        class="mb-2 w-full"
+        variant="primary"
+        icon-name="arrow-right"
+      >
+        Get in touch
+      </ButtonVariant>
+      <ButtonVariant
+        class="w-full"
+        variant="secondary"
+      >
+        Resume
+      </ButtonVariant>
+    </div>
+    <ParticlesLink />
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <SectionHero />
+  </main>
+</template>
