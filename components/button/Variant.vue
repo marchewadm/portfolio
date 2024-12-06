f7f37e0c291e6d24cd0f01b1d7bf7edba13b55bb
marchewadm <merchwa.dawid@gmail.com>
2024-12-06 23:46:42 +0100

    feat: `ButtonVariant` component with support for primary, secondary styles & optional icon integration

---
+++ components/button/Variant.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { IconName } from "~/types/icon";
+
+type Props = Partial<IconName> & {
+  variant?: "primary" | "secondary";
+};
+
+const { variant = "primary" } = defineProps<Props>();
+</script>
+
+<template>
+  <button
+    class="flex justify-center rounded-xl px-6 py-2"
+    :class="{
+      'bg-primary text-on-primary': variant === 'primary',
+      'border border-decorative text-foreground': variant === 'secondary',
+      'items-center gap-x-2': iconName,
+    }"
+  >
+    <span>
+      <slot />
+    </span>
+    <IconBase
+      v-if="iconName"
+      :icon-name="iconName"
+    />
+  </button>
+</template>
