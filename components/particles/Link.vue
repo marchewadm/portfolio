c00ea8e65b13efc98e234af2c8cf31a2145a8b34
marchewadm <merchwa.dawid@gmail.com>
2024-12-06 23:47:23 +0100

    feat: `ParticlesLink` component with particle background configuration

---
+++ components/particles/Link.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const options = {
+  fullScreen: {
+    enable: false,
+    zIndex: -1,
+  },
+  background: {
+    color: {
+      value: COLORS.surface,
+    },
+  },
+  particles: {
+    color: {
+      value: COLORS["decorative-lightest"],
+    },
+    links: {
+      color: COLORS["decorative-lightest"],
+      enable: true,
+    },
+    move: {
+      enable: true,
+    },
+    number: {
+      value: 50,
+    },
+  },
+  interactivity: {
+    events: {
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="absolute h-full w-full">
+    <NuxtParticles
+      id="ts-particles-link"
+      class="h-full w-full"
+      :options="options"
+    />
+  </div>
+</template>
