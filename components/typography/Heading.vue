533fa5aff22fc4e01397c047a1f3d1387cfc0a5a
marchewadm <merchwa.dawid@gmail.com>
2024-12-08 13:36:36 +0100

    feat: add typography components for headings, paragraphs, and titles with variant support

---
+++ components/typography/Heading.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+type Props = {
+  tag?: "h1" | "h2";
+};
+
+const { tag = "h2" } = defineProps<Props>();
+</script>
+
+<template>
+  <component
+    :is="tag"
+    class="text-2xl font-medium"
+  >
+    <slot />
+  </component>
+</template>
---
+++ components/typography/Paragraph.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+type Props = {
+  variant?: "default" | "sm" | "thin";
+};
+
+const { variant = "default" } = defineProps<Props>();
+
+const variantClasses = computed(() => ({
+  "text-base font-light": variant === "default",
+  "text-sm font-light": variant === "sm",
+  "text-sm font-thin": variant === "thin",
+}));
+</script>
+
+<template>
+  <p
+    :class="variantClasses"
+  >
+    <slot />
+  </p>
+</template>
