41e8d28a93241f8e1c9ad40169ae234f24f1e2d1
marchewadm <merchwa.dawid@gmail.com>
2024-12-09 20:38:28 +0100

    feat: add `ButtonTechStack` component with `NuxtLink` support

---
+++ components/button/TechStack.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+type Props = {
+  href?: string;
+};
+
+const { href = "#" } = defineProps<Props>();
+</script>
+
+<template>
+  <NuxtLink
+    :to="href"
+    class="
+      border border-decorative text-foreground-lighter bg-decorative-lightest
+      rounded-md px-3 py-1 text-sm font-light
+    "
+  >
+    <slot />
+  </NuxtLink>
+</template>
