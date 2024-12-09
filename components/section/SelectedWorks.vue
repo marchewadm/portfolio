685eeaa135996261396b5f43ef1077497f5880cd
marchewadm <merchwa.dawid@gmail.com>
2024-12-09 20:40:10 +0100

    feat: add `SectionSelectedWorks` component to showcase selected works

---
+++ components/section/SelectedWorks.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="mb-6 flex flex-col items-center px-4">
+    <TypographyHeading class="mb-1">
+      Selected Works
+    </TypographyHeading>
+    <TypographyParagraph
+      class="mb-3"
+      variant="thin"
+    >
+      Showcasing skills through real projects
+    </TypographyParagraph>
+    <AccordionSelectedWorks />
+    <ButtonVariant
+      class="w-full"
+      variant="secondary"
+    >
+      See all
+    </ButtonVariant>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,6 @@
   <main>
     <SectionHero />
     <SectionAbout />
+    <SectionSelectedWorks />
   </main>
 </template>
