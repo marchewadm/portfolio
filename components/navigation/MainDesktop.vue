329a63e977f85e68eb39620a0da1fae0715dce54
marchewadm <merchwa.dawid@gmail.com>
2025-01-01 20:53:06 +0100

    feat: add desktop navigation component

---
+++ components/navigation/MainDesktop.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const siteNavigationData = [
+  {
+    name: "Home",
+  },
+  {
+    name: "About",
+  },
+  {
+    name: "Blog",
+  },
+  {
+    name: "Selected Works",
+  },
+];
+</script>
+
+<template>
+  <nav
+    class="
+      hidden
+
+      md:block
+    "
+  >
+    <ul class="flex gap-x-6">
+      <NuxtLink
+        v-for="navigation in siteNavigationData"
+        :key="navigation.name"
+        class="font-light tracking-tight"
+        to="#"
+      >
+        {{ navigation.name }}
+      </NuxtLink>
+    </ul>
+  </nav>
+  <button
+    class="
+      bg-primary text-on-primary hidden rounded-xl px-6 py-2 tracking-tight
+
+      md:inline-block
+    "
+  >
+    Contact
+  </button>
+</template>
--- components/navigation/MainWrapper.vue
+++ components/navigation/MainWrapper.vue
@@ -4,9 +4,10 @@
       bg-surface/75 sticky top-0 z-50 px-4 py-6 backdrop-blur-md shadow-xs-above
     "
   >
-    <nav class="flex items-center justify-between">
+    <nav class="mx-auto flex max-w-screen-xl items-center justify-between">
       <ButtonLogo />
       <NavigationMainMobile />
+      <NavigationMainDesktop />
     </nav>
   </header>
 </template>
