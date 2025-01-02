7d3a3d4f6c41aa7dd058e556c07fcce8afa2d90f
marchewadm <merchwa.dawid@gmail.com>
2025-01-02 16:18:56 +0100

    style: enhance button variant styles with hover and focus-visible effects

--- components/button/Variant.vue
+++ components/button/Variant.vue
@@ -9,8 +9,8 @@ type Props = Partial<IconName> & {
 const { iconName, variant = "primary" } = defineProps<Props>();
 
 const variantClasses = computed(() => ({
-  "bg-primary text-on-primary": variant === "primary",
-  "border border-decorative text-foreground": variant === "secondary",
+  "bg-primary text-on-primary transition-colors duration-300 hover:bg-primary-lighter hover:text-white focus-visible:bg-primary-lighter focus-visible:text-white": variant === "primary",
+  "border border-decorative text-foreground transition-colors duration-300 hover:bg-decorative-lightest focus-visible:bg-decorative-lightest": variant === "secondary",
   "items-center gap-x-2": iconName,
 }));
 </script>
@@ -30,7 +30,7 @@ const variantClasses = computed(() => ({
     <IconBase
       v-if="iconName"
       :icon-name="iconName"
-      :class="{ 'group-hover:animate-bounce-right': isIconAnimated }"
+      :class="{ 'group-focus-visible:animate-bounce-right group-hover:animate-bounce-right': isIconAnimated }"
     />
   </button>
 </template>
