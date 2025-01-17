48a8d8d2e75b68987985be45a851eb6bd19aa3ba
marchewadm <merchwa.dawid@gmail.com>
2025-01-17 18:49:51 +0100

    feat: add `AboutHero` component for showcasing introductory content

---
+++ components/section/AboutHero.vue
@@ -0,0 +1,19 @@
+<template>
+  <SectionSubPageBase>
+    <TypographyHeading
+      class="mb-3"
+      tag="h1"
+    >
+      Behind every face is a tale of trials, triumphs, and transformation
+    </TypographyHeading>
+    <TypographyParagraph class="mb-2">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua.
+    </TypographyParagraph>
+    <TypographyParagraph class="mb-4">
+      Et harum quidem rerum facilis est et expedita distinctio. Nam libero  tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo  minus id quod maxime placeat facere possimus, omnis voluptas assumenda  est, omnis dolor repellendus. Temporibus autem quibusdam.
+    </TypographyParagraph>
+    <ButtonVariant class="w-full">
+      Resume
+    </ButtonVariant>
+  </SectionSubPageBase>
+</template>
