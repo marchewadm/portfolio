3ec28b604d09e14799a834a13ccda323324b007d
marchewadm <merchwa.dawid@gmail.com>
2025-01-17 19:04:29 +0100

    feat: add `AboutBeyondWork` component for showcasing experiences beyond professional life

---
+++ components/section/AboutBeyondWork.vue
@@ -0,0 +1,22 @@
+<template>
+  <SectionSubPageBase>
+    <TypographyHeading class="mb-3">
+      Beyond Work
+    </TypographyHeading>
+    <TypographyParagraph class="mb-3 text-justify">
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem  accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt  explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut  odit aut fugit, sed quia consequuntur magni dolores eos qui ratione  voluptatem sequi nesciunt.
+    </TypographyParagraph>
+    <ImageCaption
+      class="mb-3"
+      image-src="/img/work-1.jpg"
+      image-alt="Lorem ipsum"
+      image-caption="Photo taken from my personal collection"
+    />
+    <TypographyParagraph class="mb-2 text-justify">
+      Et harum quidem rerum facilis est et expedita distinctio. Nam libero  tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo  minus id quod maxime placeat facere possimus, omnis voluptas assumenda  est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates  repudiandae sint et molestiae non recusandae.
+    </TypographyParagraph>
+    <TypographyParagraph class="text-justify">
+      Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis  voluptatibus maiores alias consequatur aut perferendis doloribus  asperiores repellat.
+    </TypographyParagraph>
+  </SectionSubPageBase>
+</template>
