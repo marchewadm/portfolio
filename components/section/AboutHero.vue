73bae64bd1ff21d43bf52a1ae1d96dafdb7219fe
marchewadm <merchwa.dawid@gmail.com>
2025-01-20 22:06:40 +0100

    feat: display SVG hero on screen above `768 px` width

--- components/section/AboutHero.vue
+++ components/section/AboutHero.vue
@@ -1,19 +1,37 @@
 <template>
-  <SectionSubPageBase>
-    <TypographyHeading
-      class="mb-3"
-      tag="h1"
-    >
-      Behind every face is a tale of trials, triumphs, and transformation
-    </TypographyHeading>
-    <TypographyParagraph class="mb-2">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua.
-    </TypographyParagraph>
-    <TypographyParagraph class="mb-4">
-      Et harum quidem rerum facilis est et expedita distinctio. Nam libero  tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo  minus id quod maxime placeat facere possimus, omnis voluptas assumenda  est, omnis dolor repellendus. Temporibus autem quibusdam.
-    </TypographyParagraph>
-    <ButtonVariant class="w-full">
-      Resume
-    </ButtonVariant>
+  <SectionSubPageBase
+    class="md:grid md:grid-cols-[1fr_auto] md:gap-x-36"
+  >
+    <div>
+      <TypographyHeading
+        tag="h1"
+        class="mb-3"
+      >
+        Behind every face is a tale of trials, triumphs, and transformation
+      </TypographyHeading>
+      <TypographyParagraph class="mb-4">
+        Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam.
+      </TypographyParagraph>
+      <ButtonVariant
+        class="
+          w-full
+
+          md:w-fit
+        "
+        icon-name="arrow-right"
+        :is-icon-animated="true"
+      >
+        View my resume
+      </ButtonVariant>
+    </div>
+    <SvgoHeroGreeting
+      class="
+        hidden h-[430px]
+
+        md:block
+      "
+      filled
+      :font-controlled="false"
+    />
   </SectionSubPageBase>
 </template>
