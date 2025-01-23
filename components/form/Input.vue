3a5e3b29146621eeef53ee4e83621f5f5e851fea
marchewadm <merchwa.dawid@gmail.com>
2025-01-23 16:15:59 +0100

    feat: implement v-model binding in `Input` component & adjust `Label` styling

--- components/form/Input.vue
+++ components/form/Input.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+const modelValue = defineModel();
+</script>
+
 <template>
   <input
+    v-model="modelValue"
     class="
-      border p-3 border-decorative-lighter mb-4 rounded-xl font-light
+      border p-3 border-decorative-lighter rounded-xl font-light
+      placeholder-foreground-lightest
 
       focus:outline focus:outline-2 focus:outline-primary
 
