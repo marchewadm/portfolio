40dca7228f1ff4bbcb2d3fa64b28a28c6cd7dca4
marchewadm <merchwa.dawid@gmail.com>
2025-01-28 20:00:09 +0100

    feat: utilize Z-shaped design pattern for displaying portfolio entries

--- components/section/SelectedWorksPortfolio.vue
+++ components/section/SelectedWorksPortfolio.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 const route = useRoute();
 
+const isMediumScreen = useMediaQuery("(min-width: 1024px)");
+
 const { data: portfolios } = await useAsyncData(route.path, () => {
   return queryCollection("portfolio").order("createdAt", "DESC").all();
 });
@@ -12,87 +14,174 @@ const { data: portfolios } = await useAsyncData(route.path, () => {
       Selected Works
     </TypographyHeading>
     <template v-if="portfolios">
-      <article
-        v-for="(portfolio, index) in portfolios"
-        :key="portfolio.path"
-      >
-        <TypographyTitle>
-          {{ portfolio.title }}
-        </TypographyTitle>
-        <TypographyParagraph class="text-foreground-lighter">
-          {{ portfolio.subtitle }}
-        </TypographyParagraph>
-        <div
-          v-if="portfolio.demoUrl || portfolio.sourceCodeUrl"
-          class="
-            mt-2 flex flex-col gap-y-2 place-self-start
-
-            md:gap-y-3
-          "
+      <template v-if="!isMediumScreen">
+        <article
+          v-for="(portfolio, index) in portfolios"
+          :key="portfolio.path"
         >
-          <LinkIconExternal
-            v-if="portfolio.demoUrl"
-            icon-name="world-www"
-            :href="portfolio.demoUrl"
+          <TypographyTitle>
+            {{ portfolio.title }}
+          </TypographyTitle>
+          <TypographyParagraph class="text-foreground-lighter">
+            {{ portfolio.subtitle }}
+          </TypographyParagraph>
+          <div
+            v-if="portfolio.demoUrl || portfolio.sourceCodeUrl"
+            class="
+              mt-2 flex flex-col gap-y-2 place-self-start
+
+              md:gap-y-3
+            "
           >
-            Try out the demo
-          </LinkIconExternal>
-          <LinkIconExternal
-            v-if="portfolio.sourceCodeUrl"
-            icon-name="brand-github"
-            :href="portfolio.sourceCodeUrl"
+            <LinkIconExternal
+              v-if="portfolio.demoUrl"
+              icon-name="world-www"
+              :href="portfolio.demoUrl"
+            >
+              Try out the demo
+            </LinkIconExternal>
+            <LinkIconExternal
+              v-if="portfolio.sourceCodeUrl"
+              icon-name="brand-github"
+              :href="portfolio.sourceCodeUrl"
+            >
+              View source code on GitHub
+            </LinkIconExternal>
+          </div>
+          <NuxtImg
+            class="
+              mb-2 mt-3 h-52 w-full rounded-xl object-cover grayscale
+              transition-all duration-300
+
+              hover:grayscale-0
+
+              lg:h-[500px]
+
+              md:mt-4 md:h-96
+            "
+            loading="lazy"
+            :src="portfolio.image"
+            :alt="portfolio.alt"
+          />
+          <TypographyParagraph class="mb-3 text-justify">
+            {{ portfolio.description }}
+          </TypographyParagraph>
+          <ButtonVariant
+            class="
+              mx-auto w-full
+
+              md:w-fit
+            "
+            variant="secondary"
+            :href="portfolio.path"
           >
-            View source code on GitHub
-          </LinkIconExternal>
+            Read more
+          </ButtonVariant>
+          <div
+            class="
+              flex flex-wrap gap-2 pt-3
+
+              md:pt-4
+            "
+            :class="{ 'border-b border-decorative mb-3 pb-3': index !== portfolios.length - 1 }"
+          >
+            <ButtonTag
+              v-for="technology in portfolio.technologies"
+              :key="technology.name"
+              :href="technology.homepageUrl"
+              render-as="link"
+              variant="filled"
+            >
+              {{ technology.name }}
+            </ButtonTag>
+          </div>
+        </article>
+      </template>
+      <template v-else>
+        <div class="grid gap-y-12">
+          <article
+            v-for="(portfolio, index) in portfolios"
+            :key="portfolio.path"
+            class="grid grid-cols-2 grid-rows-1 gap-x-12"
+          >
+            <div
+              class="flex flex-col"
+              :class="index % 2 !== 0 ? 'order-2' : 'order-1'"
+            >
+              <TypographyTitle>
+                {{ portfolio.title }}
+              </TypographyTitle>
+              <TypographyParagraph class="text-foreground-lighter">
+                {{ portfolio.subtitle }}
+              </TypographyParagraph>
+              <div
+                v-if="portfolio.demoUrl || portfolio.sourceCodeUrl"
+                class="
+                  mt-2 flex flex-col gap-y-2 place-self-start
+
+                  md:gap-y-3
+                "
+              >
+                <LinkIconExternal
+                  v-if="portfolio.demoUrl"
+                  icon-name="world-www"
+                  :href="portfolio.demoUrl"
+                >
+                  Try out the demo
+                </LinkIconExternal>
+                <LinkIconExternal
+                  v-if="portfolio.sourceCodeUrl"
+                  icon-name="brand-github"
+                  :href="portfolio.sourceCodeUrl"
+                >
+                  View source code on GitHub
+                </LinkIconExternal>
+              </div>
+              <TypographyParagraph class="my-3 text-justify">
+                {{ portfolio.description }}
+              </TypographyParagraph>
+              <ButtonVariant
+                class="
+                  mx-auto w-full
+
+                  md:w-fit
+                "
+                variant="secondary"
+                :href="portfolio.path"
+              >
+                Read more
+              </ButtonVariant>
+            </div>
+            <div
+              class="flex flex-col"
+              :class="index % 2 !== 0 ? 'order-1' : 'order-2'"
+            >
+              <NuxtImg
+                class="
+                  mb-3 h-80 w-full rounded-xl object-cover grayscale
+                  transition-all duration-300
+
+                  hover:grayscale-0
+                "
+                loading="lazy"
+                :src="portfolio.image"
+                :alt="portfolio.alt"
+              />
+              <div class="mt-auto flex flex-wrap gap-2">
+                <ButtonTag
+                  v-for="technology in portfolio.technologies"
+                  :key="technology.name"
+                  :href="technology.homepageUrl"
+                  render-as="link"
+                  variant="filled"
+                >
+                  {{ technology.name }}
+                </ButtonTag>
+              </div>
+            </div>
+          </article>
         </div>
-        <NuxtImg
-          class="
-            mb-2 mt-3 h-52 w-full rounded-xl object-cover grayscale
-            transition-all duration-300
-
-            hover:grayscale-0
-
-            lg:h-[500px]
-
-            md:mt-4 md:h-96
-          "
-          loading="lazy"
-          :src="portfolio.image"
-          :alt="portfolio.alt"
-        />
-        <TypographyParagraph class="mb-3 text-justify">
-          {{ portfolio.description }}
-        </TypographyParagraph>
-        <ButtonVariant
-          class="
-            mx-auto w-full
-
-            md:w-fit
-          "
-          variant="secondary"
-          :href="portfolio.path"
-        >
-          Read more
-        </ButtonVariant>
-        <div
-          class="
-            flex flex-wrap gap-2 pt-3
-
-            md:pt-4
-          "
-          :class="{ 'border-b border-decorative mb-3 pb-3': index !== portfolios.length - 1 }"
-        >
-          <ButtonTag
-            v-for="technology in portfolio.technologies"
-            :key="technology.name"
-            :href="technology.homepageUrl"
-            render-as="link"
-            variant="filled"
-          >
-            {{ technology.name }}
-          </ButtonTag>
-        </div>
-      </article>
+      </template>
     </template>
   </SectionSubPageBase>
 </template>
