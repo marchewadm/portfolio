4978f45276bcea6d73f9005addfbff4bd33b87da
marchewadm <merchwa.dawid@gmail.com>
2025-01-28 18:40:01 +0100

    feat: fit `SelectedWorksHero` layout to match bigger screens & provide SVG hero illustration

--- components/section/SelectedWorksHero.vue
+++ components/section/SelectedWorksHero.vue
@@ -1,27 +1,40 @@
 <template>
-  <SectionSubPageBase>
-    <TypographyHeading
-      tag="h1"
-      class="mb-3"
-    >
-      Projects that I have worked on so far and the stories behind them
-    </TypographyHeading>
-    <TypographyParagraph class="mb-2">
-      This is where I share the projects I've completed so far, as well as those still in progress but ready to be showcased.
-    </TypographyParagraph>
-    <TypographyParagraph class="mb-4">
-      This isn't just a portfolio - it's more of a journal where I share my thoughts, challenges, and solutions throughout the creation process. Feel free to explore, and if you have any questions, don't hesitate to contact me!
-    </TypographyParagraph>
-    <ButtonVariant
-      class="
-        w-full
+  <SectionSubPageBase
+    class="md:grid md:grid-cols-[1fr_auto] md:items-center md:gap-x-36"
+  >
+    <div>
+      <TypographyHeading
+        tag="h1"
+        class="mb-3"
+      >
+        Projects that I have worked on so far and the stories behind them
+      </TypographyHeading>
+      <TypographyParagraph class="mb-2">
+        This is where I share the projects I've completed so far, as well as those still in progress but ready to be showcased.
+      </TypographyParagraph>
+      <TypographyParagraph class="mb-4">
+        This isn't just a portfolio - it's more of a journal where I share my thoughts, challenges, and solutions throughout the creation process. Feel free to explore, and if you have any questions, don't hesitate to contact me!
+      </TypographyParagraph>
+      <ButtonVariant
+        class="
+          w-full
 
-        md:w-fit
+          md:w-fit
+        "
+        icon-name="arrow-right"
+        :is-icon-animated="true"
+      >
+        Check out my GitHub
+      </ButtonVariant>
+    </div>
+    <SvgoHeroIdea
+      class="
+        hidden h-[430px]
+
+        md:block
       "
-      icon-name="arrow-right"
-      :is-icon-animated="true"
-    >
-      Check out my GitHub
-    </ButtonVariant>
+      filled
+      :font-controlled="false"
+    />
   </SectionSubPageBase>
 </template>
