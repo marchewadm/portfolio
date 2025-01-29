3f9fa688f2cb3aa4adedd650842939fe72e32095
marchewadm <merchwa.dawid@gmail.com>
2025-01-29 15:42:17 +0100

    feat: move and rename page `selected-works.vue` to `selected-works/index.vue`, provide basic functionality to render selected work post

---
+++ pages/selected-works/[slug].vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const route = useRoute();
+
+const { data: page } = await useAsyncData(route.path, () => {
+  return queryCollection("selectedWorks").path(route.path).first();
+});
+</script>
+
+<template>
+  <NuxtLayout v-if="page">
+    <SectionSubPageBase>
+      <ContentRenderer :value="page" />
+    </SectionSubPageBase>
+  </NuxtLayout>
+  <p v-else>
+    TODO: 404
+  </p>
+</template>
