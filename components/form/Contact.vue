48b14fdba6f26e814127e152a3c85d3256086194
marchewadm <merchwa.dawid@gmail.com>
2025-02-21 19:45:39 +0100

    refactor(contactSchema): move contact validation schema to a separate file for better organization

--- components/form/Contact.vue
+++ components/form/Contact.vue
@@ -1,21 +1,5 @@
 <script setup lang="ts">
-import { z } from "zod";
-
-const contactSchema = z.object({
-  city: z.string().max(0).optional(),
-  email: z
-    .string()
-    .min(1, { message: "Email is required" })
-    .email({ message: "Please enter a valid email address" }),
-  subject: z
-    .string()
-    .min(8, { message: "The subject must be at least 8 characters long" })
-    .max(64, { message: "The subject can be at most 64 characters long" }),
-  message: z
-    .string()
-    .min(32, { message: "The message must be at least 32 characters long" })
-    .max(1024, { message: "The message can be at most 1024 characters long" }),
-});
+import { contactSchema } from "~/schemas/contactSchema";
 
 const { resetForm, handleSubmit, isSubmitting } = useForm({
   validationSchema: toTypedSchema(contactSchema),
