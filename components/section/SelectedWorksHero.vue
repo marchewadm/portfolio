cf6aa96614482ec81efa11213cfd28fe1c166e27
marchewadm <merchwa.dawid@gmail.com>
2025-02-24 21:50:37 +0100

    feat: add animate on scroll effect to the `Selected Works` page

--- components/section/SelectedWorksHero.vue
+++ components/section/SelectedWorksHero.vue
@@ -6,36 +6,53 @@
       <TypographyHeading
         id="section-selected-works-hero"
         tag="h1"
-        class="mb-3"
+        class="mb-3 opacity-0"
+        data-aos="fade-up"
       >
         Projects that I have worked on so far and the stories behind them
       </TypographyHeading>
-      <TypographyParagraph class="mb-2">
+      <TypographyParagraph
+        class="mb-2 opacity-0"
+        data-aos="fade-up"
+        data-aos-delay="600"
+      >
         This is where I share the projects I've completed so far, as well as those still in progress but ready to be showcased.
       </TypographyParagraph>
-      <TypographyParagraph class="mb-4">
+      <TypographyParagraph
+        class="mb-4 opacity-0"
+        data-aos="fade-up"
+        data-aos-delay="600"
+      >
         This isn't just a portfolio - it's more of a journal where I share my thoughts, challenges, and solutions throughout the creation process. Feel free to explore, and if you have any questions, don't hesitate to contact me!
       </TypographyParagraph>
-      <ButtonVariant
+      <div
         class="
-          w-full
+          opacity-0
 
           md:w-fit
         "
-        icon-name="arrow-right"
-        icon-animation="bounce-right"
+        data-aos="fade-left"
+        data-aos-delay="1200"
       >
-        Check out my GitHub
-      </ButtonVariant>
+        <ButtonVariant
+          class="w-full"
+          icon-name="arrow-right"
+          icon-animation="bounce-right"
+        >
+          Check out my GitHub
+        </ButtonVariant>
+      </div>
     </div>
     <SvgoHeroIdea
       class="
-        hidden h-[430px]
+        hidden h-[430px] opacity-0
 
         md:block
       "
-      filled
+      data-aos="zoom-in"
+      data-aos-delay="1800"
       :font-controlled="false"
+      filled
     />
   </SectionSubPageBase>
 </template>
