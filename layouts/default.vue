1f3e287f4c23912e4d04a8d34898faa91afcd12e
marchewadm <merchwa.dawid@gmail.com>
2025-02-24 21:57:08 +0100

    fix(layout): add overflow-hidden to main container
    Added the `overflow-hidden` class to the `<main>` element in `layouts/default.vue` to prevent unnecessary scrolling during Animate on Scroll animations.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex min-h-screen flex-col">
     <NavigationMainWrapper />
-    <main class="flex flex-grow flex-col">
+    <main class="flex flex-grow flex-col overflow-hidden">
       <slot />
     </main>
     <FooterWrapper />
