a641d9ec5eb065219fa61fe37d95aba32c8d2eaa
marchewadm <merchwa.dawid@gmail.com>
2025-03-12 14:09:04 +0100

    refactor(button): split `button/Navigation` into `BaseLinkNavigation` and `LinkMuted` components
    - Refactored `button/Navigation.vue` by splitting it into two components: `BaseLinkNavigation.vue` and `LinkMuted.vue`.
    - `BaseLinkNavigation` serves as the core navigation link component, providing consistent functionality for `href`, `title`, and `target` props.
    - `LinkMuted` extends `BaseLinkNavigation` with additional styling for a muted and hoverable link.
    - Both components are part of the base and link structures for better modularity and reusability.

---
+++ components/base/link/BaseLinkNavigation.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { BaseLink } from "~/types/common";
+
+defineProps<BaseLink>();
+</script>
+
+<template>
+  <NuxtLink
+    class="font-light tracking-tight"
+    :to="href"
+    :title="title"
+    :target="target"
+  >
+    <slot />
+  </NuxtLink>
+</template>
