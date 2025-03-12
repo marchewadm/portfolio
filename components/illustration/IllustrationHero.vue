ee0e32d51b36695a4fc708b38fbc183a6a8277ec
marchewadm <merchwa.dawid@gmail.com>
2025-03-12 17:23:57 +0100

    feat(illustration): add `IllustrationHero.vue` for reusable SVG illustrations
    - Created `IllustrationHero.vue` component to improve code reusability by centralizing common styles and attributes for SVG illustrations.
    - Introduced a dynamic rendering mechanism by passing the illustration name via props.
    - Added `utils/capitalize-first-letter.ts` utility function to capitalize the first letter of a string, used for dynamic rendering of illustrations.

---
+++ components/illustration/IllustrationHero.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { Illustration } from "~/types/common";
+
+const { illustration } = defineProps<Illustration>();
+
+const asTag = computed(() => resolveComponent(`SvgoHero${capitalizeFirstLetter(illustration)}`));
+</script>
+
+<template>
+  <component
+    :is="asTag"
+    :font-controlled="false"
+    class="
+      hidden h-[430px] opacity-0
+
+      md:block
+    "
+    data-aos="zoom-in"
+    data-aos-delay="1800"
+    filled
+  />
+</template>
