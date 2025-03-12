2e3fa0d9afcfffb8354426e08db302d3fb951662
marchewadm <merchwa.dawid@gmail.com>
2025-03-12 19:35:13 +0100

    refactor(selected-works): refactor Selected Works hero section and update folder structure
    - Rename component: `section/SelectedWorksHero.vue` → `section/selected-works/SectionSelectedWorksHero.vue`
    - Update hero section to use `<BaseSectionSubPageHero>` instead of `<SectionSubPageBase>`.
    - Replace `<TypographyHeading>` with heading props (`heading` and `heading-id`).
    - Replace `<ButtonVariant>` with `<ButtonVariantWithIcon>`.

---
+++ components/section/selected-works/SectionSelectedWorksHero.vue
@@ -0,0 +1,41 @@
+<template>
+  <BaseSectionSubPageHero
+    heading="Projects that I have worked on so far and the stories behind them"
+    heading-id="section-selected-works-hero"
+    illustration="idea"
+  >
+    <TypographyParagraph
+      class="mb-2 opacity-0"
+      data-aos="fade-up"
+      data-aos-delay="600"
+    >
+      This is where I share the projects I've completed so far, as well as those still in progress but ready to be showcased.
+    </TypographyParagraph>
+    <TypographyParagraph
+      class="mb-4 opacity-0"
+      data-aos="fade-up"
+      data-aos-delay="600"
+    >
+      This isn't just a portfolio - it's more of a journal where I share my thoughts, challenges, and solutions throughout the creation process. Feel free to explore, and if you have any questions, don't hesitate to contact me!
+    </TypographyParagraph>
+    <div
+      class="
+        opacity-0
+
+        md:w-fit
+      "
+      data-aos="fade-left"
+      data-aos-delay="1200"
+    >
+      <ButtonVariantWithIcon
+        class="w-full"
+        icon-name="arrow-right"
+        animation="bounce-right"
+        href="#"
+        title="Visit my GitHub profile in a new tab"
+      >
+        Check out my GitHub
+      </ButtonVariantWithIcon>
+    </div>
+  </BaseSectionSubPageHero>
+</template>
