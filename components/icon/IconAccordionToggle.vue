0b6409b4b2fe01612ed2083083341c0f6b04c17e
marchewadm <merchwa.dawid@gmail.com>
2025-03-12 16:09:22 +0100

    feat(icon): add `IconAccordionToggle` component
    - Introduced `IconAccordionToggle.vue` to provide a reusable chevron icon for accordion components.
    - The icon rotates 180 degrees when the accordion is open.
    - Improves reusability of the accordion toggle icon across the project.

---
+++ components/icon/IconAccordionToggle.vue
@@ -0,0 +1,10 @@
+<template>
+  <BaseIcon
+    icon-name="chevron-down"
+    class="
+      text-decorative transition-transform duration-300
+
+      group-data-[state=open]:rotate-180
+    "
+  />
+</template>
