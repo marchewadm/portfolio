1a38f64c04197e3f85d99d215f02e93f059b1436
marchewadm <merchwa.dawid@gmail.com>
2025-03-12 18:54:27 +0100

    refactor(content): replace `LinkIconExternal` with `ListLinkDemoAndSource` in `ArticleFooterPortfolio.vue`
    - Replaced individual `LinkIconExternal` components with `ListLinkDemoAndSource` for better maintainability.
    - Improved code reusability by utilizing the new component for demo and source code links.

--- components/content/ArticleFooterPortfolio.vue
+++ components/content/ArticleFooterPortfolio.vue
@@ -8,29 +8,9 @@ defineProps<Props>();
 </script>
 
 <template>
-  <div
-    v-if="demoUrl || sourceCodeUrl"
-    class="
-      border-t border-decorative mt-3 flex flex-col gap-y-2 pt-3
-
-      md:gap-y-3
-    "
-  >
-    <LinkIconExternal
-      v-if="demoUrl"
-      href="#"
-      icon-name="world-www"
-      title="Try out the demo in a new tab"
-    >
-      Try out the demo
-    </LinkIconExternal>
-    <LinkIconExternal
-      v-if="sourceCodeUrl"
-      href="#"
-      icon-name="brand-github"
-      title="Explore the source code on GitHub in a new tab"
-    >
-      View source code on GitHub
-    </LinkIconExternal>
-  </div>
+  <ListLinkDemoAndSource
+    :demo-url="demoUrl"
+    :source-code-url="sourceCodeUrl"
+    :bordered="true"
+  />
 </template>
