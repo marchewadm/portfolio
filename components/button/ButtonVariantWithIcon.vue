0af8a8ccb764dd8775bb7aa5bd89d6a448156d0c
marchewadm <merchwa.dawid@gmail.com>
2025-03-12 12:46:03 +0100

    refactor: split `ButtonVariant` component into `BaseButtonVariant` and `ButtonVariantWithIcon`
    Refactored `button/Variant.vue` by extracting its core functionality into `base/button/BaseButtonVariant.vue`.
    Created `button/ButtonVariantWithIcon.vue` to handle buttons with icons separately, improving reusability and maintainability.
    Updated prop handling and removed redundant logic.

---
+++ components/button/ButtonVariantWithIcon.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { BaseButtonVariant, IconName } from "~/types/common";
+
+type Props = BaseButtonVariant & IconName & {
+  animation?: "bounce-right" | "spin";
+};
+
+const { animation } = defineProps<Props>();
+
+const animationClasses = computed(() => ({
+  "group-focus-visible:animate-bounce-right group-hover:animate-bounce-right": animation === "bounce-right",
+  "animate-spin": animation === "spin",
+}));
+</script>
+
+<template>
+  <BaseButtonVariant
+    class="group items-center gap-x-2"
+    :href="href"
+    :title="title"
+    :variant="variant"
+    :target="target"
+  >
+    <span>
+      <slot />
+    </span>
+    <BaseIcon
+      :icon-name="iconName"
+      :class="animationClasses"
+    />
+  </BaseButtonVariant>
+</template>
