d2b1994d0d2e5e073ca71291c72ebcbc951173c6
marchewadm <merchwa.dawid@gmail.com>
2025-09-13 21:08:47 +0200

    refactor: temporarily disable contact form for GDPR compliance

--- server/api/contact.post.ts
+++ server/api/contact.post.ts
@@ -2,10 +2,10 @@ import { contactSchema } from "~/schemas/contactSchema";
 
 export default defineEventHandler(async (event) => {
   const successMessage = "Message sent successfully! I will get back to you as soon as possible." as const;
+  const placeholderMessage = "Currently not accepting messages - working on GDPR compliance." as const;
 
   const result = await readValidatedBody(event, body => contactSchema.safeParse(body));
 
-  // If the body is invalid, return an error response.
   if (!result.success) {
     // If the body contains filled honeypot field named `city`, it's probably a bot and the request should be ignored. It returns a 200 status code on purpose to avoid revealing the honeypot field to the bot.
     if (result.error.formErrors.fieldErrors.city) {
@@ -19,8 +19,9 @@ export default defineEventHandler(async (event) => {
     }
   }
 
-  // If the body is valid, send the email.
-  // TODO: Implement sending the email here.
-
-  return { message: successMessage };
+  throw createError({
+    statusCode: 400,
+    statusMessage: placeholderMessage,
+  });
+  // return { message: successMessage };
 });
