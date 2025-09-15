ea3119d2ccff01d63478c611a6348ac48dfe5f4e
marchewadm <merchwa.dawid@gmail.com>
2025-09-15 17:27:00 +0200

    fix(CodeBlock): reduce padding from 6 to 4 for improved layout

--- components/content/CodeBlock.vue
+++ components/content/CodeBlock.vue
@@ -1,13 +1,10 @@
 <template>
   <div
     class="
-      mb-2 flex overflow-x-auto rounded-lg border bg-[#F8FAFCFF] p-6
+      mb-2 flex overflow-x-auto rounded-lg border bg-[#F8FAFCFF] p-4
       border-decorative text-base
     "
   >
     <slot />
   </div>
 </template>
-
-<!-- TODO: Maybe consider adding something like title of the code block or at least the lang that it is created in? -->
-<!-- TODO: Add copy button as well. -->
